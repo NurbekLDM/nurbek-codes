8082bb17b5dd7451ba8fc8273fda0dcc48390c33
NurbekLDM <jumaboyevabarno80@gmail.com>
2025-03-11 16:27:00 +0500

    Fixed some issues in admin.js

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -148,14 +148,14 @@ router.post("/refresh", async (req, res) => {
 // api/admin/face-login
 router.post("/face-login", async (req, res) => {
   try {
-    const isDevelopment = process.env.NODE_ENV === "development";
+
     
     const token = jwt.sign({ user: "face_user" }, JWT_SECRET, { expiresIn: "1h" });
 
     res.setHeader("Set-Cookie", serialize("accessToken", token, {
       httpOnly: true,
-      secure: !isDevelopment,
-      sameSite: isDevelopment ? "lax" : "none", 
+      secure: true,
+      sameSite: "none", 
       maxAge: 3600,
       path: "/",
     }));
