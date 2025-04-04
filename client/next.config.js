f63ca21f8be849cf0c9f2c07523f257a66852db3
NurbekLDM <jumaboyevabarno80@gmail.com>
2025-04-04 17:42:30 +0500

    Some changes

--- client/next.config.js
+++ client/next.config.js
@@ -17,27 +17,4 @@ const nextConfig = {
   },
 };
 
-const honeybadgerNextJsConfig = {
-
-  disableSourceMapUpload: false,
-
-
-  silent: true,
-
-
-  webpackPluginOptions: {
-      apiKey: process.env.NEXT_PUBLIC_HONEYBADGER_API_KEY,
-      revision: process.env.NEXT_PUBLIC_HONEYBADGER_REVISION,
-      endpoint: 'https://api.honeybadger.io/v1/source_maps',
-      ignoreErrors: false,
-      retries: 3,
-      workerCount: 5,
-      deploy: {
-          environment: process.env.NEXT_PUBLIC_VERCEL_ENV || process.env.VERCEL_ENV || process.env.NODE_ENV,
-          repository: 'https://github.com/NurbekLDM/nurbek-codes.git',
-          localUsername: 'NurbekLDM'
-      }
-  }
-}
-
-module.exports = {nextConfig, ...honeybadgerNextJsConfig}
+module.exports = nextConfig;
